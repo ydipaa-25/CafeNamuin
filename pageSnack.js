8f04557d97f6a5dcbacf5c0f841745354881fefa
ydipaa-25 <ydipaa@gmail.com>
2026-06-17 22:11:36 +0700

    Create pageSnack.js

---
+++ pageSnack.js
@@ -0,0 +1,76 @@
+const menuItems = document.querySelectorAll(".menu-item");
+
+menuItems.forEach(item => {
+
+    const minus = item.querySelector(".minus");
+    const plus = item.querySelector(".plus");
+    const jumlah = item.querySelector(".jumlah");
+
+    plus.addEventListener("click", () => {
+
+        let qty = parseInt(jumlah.textContent);
+        qty++;
+
+        jumlah.textContent = qty;
+
+        updateKeranjang(item, qty);
+    });
+
+    minus.addEventListener("click", () => {
+
+        let qty = parseInt(jumlah.textContent);
+
+        if(qty > 0){
+            qty--;
+        }
+
+        jumlah.textContent = qty;
+
+        updateKeranjang(item, qty);
+    });
+
+});
+
+function updateKeranjang(item, qty){
+
+    let keranjang =
+        JSON.parse(localStorage.getItem("keranjang")) || [];
+
+    const nama = item.dataset.nama;
+    const harga = Number(item.dataset.harga);
+    const gambar = item.dataset.gambar;
+
+    const index = keranjang.findIndex(
+        produk => produk.nama === nama
+    );
+
+    if(qty > 0){
+
+        if(index !== -1){
+
+            keranjang[index].qty = qty;
+
+        }else{
+
+            keranjang.push({
+                nama,
+                harga,
+                gambar,
+                qty
+            });
+
+        }
+
+    }else{
+
+        if(index !== -1){
+            keranjang.splice(index,1);
+        }
+
+    }
+
+    localStorage.setItem(
+        "keranjang",
+        JSON.stringify(keranjang)
+    );
+}
